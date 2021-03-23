731a150a6a6db98bb0dfdfc2a40d1f12d889ab9c
暮鼓晨钟 <>
2021-03-23 17:20:52 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -12,7 +12,8 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org" target="_blank"
+          href="https://reactjs.org"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
